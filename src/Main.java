9bac41808a08116a8bb5e5ed61eae4373d6a6b8a
Miwwo <miwwoHG@gmail.com>
2024-01-18 22:48:57 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int singleNumber(int[] nums) {
+        int num = 0;
+        for (int i = 0; i < nums.length; i++) {
+            num = num ^ nums[i]; // XOR
+        }
+        return num;
+    }
+}
+
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
